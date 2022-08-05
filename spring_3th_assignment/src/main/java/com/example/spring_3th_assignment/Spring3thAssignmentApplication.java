158f7519ef1d92729e67dd5dda36ede8f2330157
hosung <winsomed96@naver.com>
2022-08-05 14:20:22 +0900

    test commit

---
+++ spring_3th_assignment/src/main/java/com/example/spring_3th_assignment/Spring3thAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_3th_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3thAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3thAssignmentApplication.class, args);
+    }
+
+}
